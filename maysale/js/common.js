7be882aeba900afa402bf0b18f8c25e3ebffebef
hunter <hunter.eztravel@gamil.xom>
2018-07-28 10:46:13 +0800

    年中大促 飛玩不可 機票最高單程省$600

---
+++ maysale/js/common.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+	$('.sp-header') && $('.sp-header').append(
+		"<div class='container'><div title='ezTravel 易遊網' class='logo'></div></div>"
+	);
+
+	$('.sp-footer') && $('.sp-footer').append(
+		"<div class='container'><div class='info'>Copyright&nbsp;<i class='icon-copy'>&copy;</i>2018&nbsp;ezTravel&nbsp;Co.,&nbsp;Ltd.&nbsp;All&nbsp;rights reserved.</div></div>"
+	);
+});
